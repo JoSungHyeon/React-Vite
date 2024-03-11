1b4a96fbd25aeb498deebfcf4d1da9b5d5e1920e
JoSungHyeon <jsh950618@gmail.com>
2024-03-11 11:15:02 +0900

    commit

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # React + Vite
 
 Vite 를 활용한 React Review
+
+useRef
+- Reference 객체를 생성
+- 컴포넌트 내부의 변수로 활용 가능
+- 어떤 경우에도 리랜더링 유발하지 않음
+
+useState
+- State를 생성
+- 컴포넌트 내부의 변수로 활용 가능
+- 값이 변경되면 컴포넌트 리랜더링
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -6,55 +6,56 @@ import { useState } from "react"
 // 4. 자기소개
 
 const Register = () => {
-    const [name, setName] = useState("이름");
-    const [birth, setBirth] = useState("");
-    const [country, setCountry] = useState("");
-    const [bio, setBio] = useState("")
+    const [input, setInput] = useState({
+        name: "",
+        birth: "",
+        country: "",
+        bio: ""
+    });
 
-    const onChangeName = (e) => {
-        setName(e.target.value);
+    const onChange = (e) => {
+        console.log(e.target.name, e.target.value);
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        });
     };
-
-    const onChangeBirth = (e) => {
-        setBirth(e.target.value);
-    }
-
-    const onChangeCountry = (e) => {
-        setCountry(e.target.value);
-    }
-
-    const onChangeBio = (e) => {
-        setBio(e.target.value);
-    }
 
     return (
         <div>
             <div>
                 <input
-                    value={name}
-                    onChange={onChangeName}
+                    name="name"
+                    value={input.name}
+                    onChange={onChange}
                     placeholder={"이름"}
                 />
             </div>
             <div>
                 <input
-                    value={birth}
-                    onChange={onChangeBirth}
+                    name="birth"
+                    value={input.birth}
+                    onChange={onChange}
                     type="date" />
             </div>
             <div>
-                <select value={country} onChange={onChangeCountry}>
+                <select
+                    name="country"
+                    value={input.country}
+                    onChange={onChange}>
                     <option></option>
                     <option value="kr">한국</option>
                     <option value="us">미국</option>
                     <option value="uk">영국</option>
                 </select>
-                {country}
             </div>
             
             <div>
-                <textarea value={bio} onChange={onChangeBio} />
-                {bio}
+                <textarea
+                    name="bio"
+                    value={input.bio}
+                    onChange={onChange}
+                />
             </div>
         </div>
     )
