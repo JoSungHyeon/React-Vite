074572591d90950cffc568b6204a2354957dbe1c
JoSungHyeon <jsh950618@gmail.com>
2024-03-11 10:49:59 +0900

    register commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-import { useState } from 'react';
+import Register from './components/Register'
 
 
 function App() {
 
   return (
     <>
-
+      <Register />
     </>
   )
 }
